d310663813bf6e521e65997c3f26d4547e86890b
filipHartman <filip.hartman23@gmail.com>
2018-02-11 14:44:52 +0100

    Add constructor of workaddress class

--- src/AdressBook/WorkAddress.java
+++ src/AdressBook/WorkAddress.java
@@ -1,4 +1,11 @@
 package AdressBook;
 
-public class WorkAddress {
+public class WorkAddress extends Address {
+    private String company;
+
+    public WorkAddress(String name, String city, String street, String houseNo, String company) {
+        super(name, city, street, houseNo);
+        this.company = company;
+    }
+    
 }
