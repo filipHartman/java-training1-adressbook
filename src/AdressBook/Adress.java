a5d9b8ce616c6d9e18758df25c023d938cab40e2
filipHartman <filip.hartman23@gmail.com>
2018-02-11 12:36:08 +0100

    Add constructor of class

--- src/AdressBook/Adress.java
+++ src/AdressBook/Adress.java
@@ -1,4 +1,15 @@
 package AdressBook;
 
 public class Adress {
+    private String person;
+    private String city;
+    private String street;
+    private String houseNo;
+
+    public Adress(String person, String city, String street, String houseNo) {
+        this.person = person;
+        this.city = city;
+        this.street = street;
+        this.houseNo = houseNo;
+    }
 }
