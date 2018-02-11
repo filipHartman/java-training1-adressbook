21781a9d3a55b5e743d416d50cf22b631efdb4d3
filipHartman <filip.hartman23@gmail.com>
2018-02-11 15:35:56 +0100

    Initalize AddressBook class

--- src/AdressBook/AddressBook.java
+++ src/AdressBook/AddressBook.java
@@ -1,4 +1,12 @@
 package AdressBook;
 
+import java.util.ArrayList;
+
 public class AddressBook {
+    private String name;
+    private ArrayList<Address> addresses;
+
+    public AddressBook(String name) {
+        this.name = name;
+    }
 }
