73e847d9894963b7f62495be8b8c801d4a9bb735
filipHartman <filip.hartman23@gmail.com>
2018-02-11 16:07:37 +0100

    Add finding addresses by result phrase

--- src/AdressBook/AddressBook.java
+++ src/AdressBook/AddressBook.java
@@ -1,10 +1,12 @@
 package AdressBook;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AddressBook {
     private String name;
-    private ArrayList<Address> addresses;
+    private ArrayList<Address> addresses = new ArrayList<>();
 
     public AddressBook(String name) {
         this.name = name;
@@ -14,5 +16,18 @@ public class AddressBook {
         addresses.add(address);
     }
 
-    
+    public ArrayList<Address> find(String searchPhrase) throws IndexOutOfBoundsException {
+        ArrayList<Address> result = new ArrayList<>();
+        String pattern = ".*" + searchPhrase.toUpperCase() + ".*";
+        Pattern p = Pattern.compile(pattern);
+        for (Address address : addresses) {
+            String fullAddress = address.getFullAddress();
+            fullAddress = fullAddress.toUpperCase();
+            Matcher m = p.matcher(fullAddress);
+            if (m.matches()) {
+                result.add(address);
+            }
+        }
+        return result;
+    }
 }
