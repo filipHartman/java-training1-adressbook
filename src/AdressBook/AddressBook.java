eaf2a44dc8c46f57802d64f0bbde1a96bfa1ceb2
filipHartman <filip.hartman23@gmail.com>
2018-02-12 23:20:28 +0100

    Add creating addressbook from csv file

--- src/AdressBook/AddressBook.java
+++ src/AdressBook/AddressBook.java
@@ -1,5 +1,9 @@
 package AdressBook;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -76,5 +80,47 @@ public class AddressBook {
         Address temp = addresses.get(i);
         addresses.set(i, addresses.get(j));
         addresses.set(j, temp);
+    }
+
+    public static AddressBook createFromCSV(String listName, String csvPath) {
+        AddressBook addressBook = new AddressBook(listName);
+
+        try{
+            BufferedReader csvFile = new BufferedReader(new FileReader(csvPath));
+            csvFile.readLine();
+            String line = null;
+            while((line = csvFile.readLine()) != null) {
+                Address address = createAddressFromCSVData(line);
+                addressBook.addAddress(address);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return addressBook;
+    }
+
+    private static Address createAddressFromCSVData(String line) {
+        int personIndex = 0;
+        int cityIndex = 1;
+        int streetIndex = 2;
+        int houseNoIndex = 3;
+        int companyIndex = 4;
+
+        Address address = null;
+        String [] data = line.split(",");
+        String person = data[personIndex];
+        String city = data[cityIndex];
+        String street = data[streetIndex];
+        String houseNo = data[houseNoIndex];
+        if (data.length == 4){
+            address = new Address(person, city, street, houseNo);
+        } else if (data.length == 5) {
+            String company = data[companyIndex];
+            address = new WorkAddress(person, city, street, houseNo, company);
+        }
+        return address;
     }
 }
